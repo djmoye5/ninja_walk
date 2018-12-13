1ffd6763b22479a08335fcf1531950109c187707
Denerius Moye <dmoye@techlaunch.io>
2018-12-13 18:35:11 -0500

    https://github.com/djmoye5/ninja_walk.git

---
+++ script.js
@@ -0,0 +1,41 @@
+var keyframe1 = [
+    { left: "0px" },
+    { left: "200px" }
+];
+
+var keyframe2 = [
+    { left: "0px" },
+    { left: "400px" }
+];
+
+var keyframe3 = [
+    { left: "0px" },
+    { left: "600px" }
+];
+
+var timing = {
+    duration: 1000,
+    easing: "ease-in"
+  };
+
+var box1 = document.getElementById("box1");
+var box2 = document.getElementById("box2");
+var box3 = document.getElementById("box3");
+
+function hoverALL(){
+box1.animate(keyframe1, timing);
+box2.animate(keyframe2, timing);
+box3.animate(keyframe3, timing);
+}
+
+function hover1(){
+    box1.animate(keyframe1, timing);
+}
+
+function hover2(){
+    box2.animate(keyframe2, timing);
+}
+
+function hover3(){
+    box3.animate(keyframe3, timing);
+}
---
+++ sprite.js
@@ -0,0 +1,24 @@
+var tID;
+
+function stopAnimate(){
+    clearInterval(tID);
+}
+
+function ninjaWalk() {
+  var position = 160
+  const interval = 100
+  const diff = 160
+
+tID = setInterval(() => {
+  document.getElementById("withNinja").style.backgroundPosition +=
+  `-${position}px 0px`;
+  if (position < 1280) {
+      position = position + diff;
+  }
+  else {
+      position = 160;
+  }
+}, interval);
+}
+
+
